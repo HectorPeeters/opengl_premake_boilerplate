9651d3b37d6b4769839c8100a788a84e754cbd11
Hector Peeters <hector.peeters@gmail.com>
2020-04-30 18:40:26 +0200

    Remove old rendering code

--- src/main.cpp
+++ src/main.cpp
@@ -14,24 +14,6 @@ int main()
     Window window("OpenGL Boilerplate!!!", 960, 540, true);
     window.Create();
 
-    // Setup model
-
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
-
-    unsigned int vao;
-    GL(glGenVertexArrays(1, &vao));
-    GL(glBindVertexArray(vao));
-    unsigned int vbo;
-    GL(glGenBuffers(1, &vbo));
-    GL(glBindBuffer(GL_ARRAY_BUFFER, vbo));
-    GL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
-
-    GL(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0));
-    GL(glEnableVertexAttribArray(0));
-
     Shader shader("Basic", "res/basic.vert", "res/basic.frag");
     if (!shader.Create())
         return 1;
@@ -64,11 +46,6 @@ int main()
         }
 
         renderer.End();
-
-        // shader.Bind();
-        // GL(glBindVertexArray(vao));
-        // GL(glDrawArrays(GL_TRIANGLES, 0, 3));
-        // shader.Unbind();
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
