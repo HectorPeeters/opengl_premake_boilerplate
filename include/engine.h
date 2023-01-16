66f09bf3dd68859e0d6786ce2e8ee9332719b6e1
cogilv25 <calumlindsay.434c@gmail.com>
2023-01-16 15:30:36 +0000

    updated engine.h to use new imgui headers

--- include/engine.h
+++ include/engine.h
@@ -1,6 +1,6 @@
 #include <imgui.h>
-#include <examples/imgui_impl_glfw.h>
-#include <examples/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include <glad/glad.h>
 
